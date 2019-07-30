3900c0b68f570ec7fed3427640a7e203cdd5d663
Juliet <jnwagwuume-ezeoke@college.harvard.edu>
2019-07-30 19:17:11 +0000

    more fields

--- django_mech/mech_app/models.py
+++ django_mech/mech_app/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_mysql.models import ListCharField
+from phonenumber_field.modelfields import PhoneNumberField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-STATUS = ((0, "Pending Service"), 
-            (1, "Serviced")
+STATUS = ((0, "Pending"), 
+            (1, "Ongoing"),
+            (2, "Completed")
         )
 
 class ServiceRequest(models.Model):
@@ -12,7 +17,23 @@ class ServiceRequest(models.Model):
     title = models.CharField(max_length=30)
     complaint = models.TextField("Complaint")
     author = models.ForeignKey(User, on_delete= models.CASCADE)
-    status = models.IntegerField(choices=STATUS, default=0)
+    departments = models.TextField("Department", default="[Mechanical, Process]")
+    plant = models.CharField(max_length=20, default="Mudor")
+    status = models.PositiveSmallIntegerField(choices=STATUS, default=0)
+
+class UserDetails(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = PhoneNumberField()
+
+@receiver(post_save, sender=User)
+def create_user_details(sender, instance, created, **kwargs):
+    if created:
+        UserDetails.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.UserDetails.save()
+
 
 
 
--- django_mech/mech_project/settings.py
+++ django_mech/mech_project/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'mech_app',
     'corsheaders',
     'rest_framework.authtoken', 
-    'rest_auth',               
+    'rest_auth', 
+    'django_mysql',
+    'phonenumber_field',              
 ]
 
 MIDDLEWARE = [
@@ -88,6 +90,16 @@ DATABASES = {
         'PASSWORD': '29Chinedu!',
         'HOST': 'localhost',   
         'PORT': '3306',
+        'OPTIONS': {
+            # Tell MySQLdb to connect with 'utf8mb4' character set
+            'charset': 'utf8mb4',
+        },
+        # Tell Django to build the test database with the 'utf8mb4' character set
+        'TEST': {
+            'CHARSET': 'utf8mb4',
+            'COLLATION': 'utf8mb4_unicode_ci',
+        }
+ 
     }    
 }
 
