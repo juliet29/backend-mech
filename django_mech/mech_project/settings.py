2abdb382e08fe6fe3902829f95fcc731d743a0cf
Juliet <jnwagwuume-ezeoke@college.harvard.edu>
2019-08-06 19:26:57 +0100

    rm charset

--- django_mech/mech_project/settings.py
+++ django_mech/mech_project/settings.py
@@ -96,17 +96,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '29Chinedu!',
         'HOST': 'localhost',   
-        'PORT': '3306',
-        'OPTIONS': {
-            # Tell MySQLdb to connect with 'utf8mb4' character set
-            'charset': 'utf8mb4',
-        },
-        # Tell Django to build the test database with the 'utf8mb4' character set
-        'TEST': {
-            'CHARSET': 'utf8mb4',
-            'COLLATION': 'utf8mb4_unicode_ci',
-        }
- 
+        'PORT': '3306'
     }    
 }
 
